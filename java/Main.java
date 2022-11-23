225398a7c1cc385f0dade6773ce3f6b0823f6421
faouzilha06 <118526895+faouzilha06@users.noreply.github.com>
2022-11-23 09:28:57 +0100

    Create Main.java

---
+++ java/Main.java
@@ -0,0 +1,17 @@
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        int myNum = 5;
+        Float myFloatNum = 5.99f;
+        char myLetter ='D';
+        boolean myBool = true;
+        String myText = "Hello";
+        
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println (myBool);
+        System.out.println(myText);
+   
+    }
+} 
